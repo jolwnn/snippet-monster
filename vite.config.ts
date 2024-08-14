0b37b532210384aaff124119ada44b6d5fac8623
jolwnn <zhuolinwee@gmail.com>
2024-08-14 22:22:52 +0800

    remove faulty github deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/snippet-monster/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
