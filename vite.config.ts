e4fb82881ede38e38024e09760a7d879c6e98cc2
jolwnn <zhuolinwee@gmail.com>
2024-08-14 22:03:08 +0800

    feature: github deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/snippet-monster/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
